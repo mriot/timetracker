26f30965ee179c877059a599808af8154be2c03b
Markus <kremer.markus1996@gmail.com>
2024-09-12 20:16:23 +0200

    add debounce action

---
+++ src/lib/actions/debounce.ts
@@ -0,0 +1,34 @@
+import type { Action } from "svelte/action";
+
+type DebounceOptions = {
+    delay?: number;
+    eventTypeToDebounce: string;
+};
+
+/**
+ * Svelte action that debounces an event.
+ *
+ * Example usage: use:debounce={{ delay: 300, eventTypeToDebounce: "keydown" }} on:debounced={eventHandler}
+ *
+ * This will debounce the keydown event on the element this action is bound to.
+ * If the delay is exceeded, a custom "debounced" event will be dispatched.
+ */
+export const debounce: Action<HTMLElement, DebounceOptions> = (node: HTMLElement, options: DebounceOptions) => {
+    let timeoutId: number;
+
+    const handleEvent = (event: Event) => {
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            node.dispatchEvent(new CustomEvent("debounced", { detail: event }));
+        }, options.delay ?? 300);
+    };
+
+    node.addEventListener(options.eventTypeToDebounce, handleEvent);
+
+    return {
+        destroy() {
+            node.removeEventListener(options.eventTypeToDebounce, handleEvent);
+        },
+    };
+};
